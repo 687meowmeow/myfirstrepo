6f4a93a2b9d44a46469d8fd56ba11253f6c376fe
687meowmeow <iamkarlbirger@gmail.com>
2025-04-02 09:33:47 -0500

    what?

--- utils/client.js
+++ utils/client.js
@@ -1,23 +1,3 @@
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-// import 'firebase/database';
-
-// const firebaseCredentials = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-// };
-
-// const clientCredentials = {
-//   ...firebaseCredentials,
-//   databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-// };
-
-// if (!firebase.apps.length) {
-//   firebase?.initializeApp(firebaseCredentials);
-// }
-
-// export { firebase, clientCredentials };
-
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
